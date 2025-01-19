0d036e785da99ffa688c0b895ebf5d2d86ce8955
1nsaneYMD <trafficker.shadia@gmail.com>
2025-01-20 01:55:30 +0200

    Add requirements and default args

--- reworked_version/main_dag.py
+++ reworked_version/main_dag.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from services.airflow_operator import AirflowOperator
 
@@ -6,7 +6,11 @@ with DAG(
     dag_id='spotify_analysis_pipeline',
     description='A pipeline to analyze Spotify track data',
     start_date=datetime(2025, 1, 1),
-    catchup=False
+    catchup=False,
+    default_args={
+        'retries': 3,
+        'retry_delay': timedelta(minutes=5)
+    },
 ) as dag:
     data_pipeline = AirflowOperator(dag)
     data_pipeline.run_pipeline()
